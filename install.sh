3ff8fab3123c2a077c1710d835566a16286eb71f
FabioC-alt <fabiociraci41@gmail.com>
2025-02-19 12:35:24 +0100

    Install script

--- install.sh
+++ install.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# Interrompe lo script se una qualsiasi operazione fallisce
-set -e
-
-# Funzione per visualizzare un messaggio di errore e interrompere lo script
-error_exit() {
-    echo "Errore: $1"
-    exit 1
-}
-
-# Assicurati che lo script venga eseguito come root
-if [[ $EUID -ne 0 ]]; then
-    error_exit "Questo script deve essere eseguito come root"
-fi
-
 # Lista dei pacchetti
-packages=(
+packages='
+    stow
     wl-clipboard
     grim
     slurp
@@ -28,34 +15,30 @@ packages=(
     bluez-utils
     blueman
     mcfly
-)
+    kitty
+    firefox
+'
 
-# Aggiorna il database dei pacchetti e installa i pacchetti
-if ! pacman -Syu --needed "${packages[@]}" --noconfirm; then
-    error_exit "Errore nell'installazione dei pacchetti"
-fi
+echo 'Installing Packages'
+sudo pacman -S $packages --noconfirm
 
-# Esegui il "stow" per ogni cartella nella directory, gestendo 'chrome' separatamente
-for dir in */; do
-    if [[ "$dir" == "chrome/" ]]; then
-        target_dir="$HOME/.mozilla/firefox/$(ls $HOME/.mozilla/firefox | grep '.default-release')/chrome"
-    else
-        target_dir="$HOME/.config/${dir%/}"
-    fi
-    
-    # Crea la directory di destinazione se non esiste
-    if ! mkdir -p "$target_dir"; then
-        error_exit "Errore nella creazione della directory: $target_dir"
-    fi
+echo 'Creating Config Folders'
+mkdir -p $HOME/.config/hypr
+mkdir -p $HOME/.config/waybar
+mkdir -p $HOME/.config/wofi
 
-    # Esegui stow per creare i link simbolici
-    if ! stow -t "$target_dir" "$dir"; then
-        error_exit "Errore nell'eseguire stow per la directory: $dir"
-    fi
-done
+echo 'Stowing Folders'
+sudo stow -t $HOME/.config/hypr hypr
+sudo stow -t $HOME/.config/waybar waybar
+sudo stow -t $HOME/.config/wofi wofi
 
-# Copio contenuto di bashrc nella Home
-
+echo 'Configuring bashrc'
 cp bashrc ~/.bashrc
+source ~/.bashrc
+
+echo 'Configuring Github Credentials'
+git config --global user.email "fabiociraci41@gmail.com"
+git config --global user.name "FabioC-alt"
+
 
 echo "Script completato con successo!"
