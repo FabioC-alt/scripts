d5af756a82b62e319f814a75f499db18263cad15
FabioC-alt <fabiociraci41@gmail.com>
2025-04-07 22:57:02 +0200

    Auto-commit on 2025-04-07 22:57:02

--- allScripts/gitSync.sh
+++ allScripts/gitSync.sh
@@ -6,13 +6,13 @@ PARENT_DIR="$HOME/Documents"  # Use current directory if no argument is given
 notify-send -t 2000 "🔄 Syncing Git repositories inside: $PARENT_DIR"
 
 for dir in "$PARENT_DIR"/*/; do
-  if [ -d "$dir/.git" ]; then
+  if [[ $dir =~ ^[a-z] ]] && [ -d "$dir/.git" ]; then
     cd "$dir" || continue
     git remote update
     git pull
     cd - >/dev/null || exit
   else
-    notify-send -t 2000 "❌ Skipping $dir (not a git repo)"
+    notify-send -t 5000 "❌ Skipping $dir (not a git repo)"
   fi
 done
 
