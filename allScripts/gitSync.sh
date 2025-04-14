b9dab90a1340c884f3f6a11af04793e5d2e7557a
FabioC-alt <fabiociraci41@gmail.com>
2025-04-14 20:02:26 +0200

    Auto-commit on 2025-04-14 20:02:26

--- allScripts/gitSync.sh
+++ allScripts/gitSync.sh
@@ -3,7 +3,7 @@
 # Set the parent directory
 PARENT_DIR="$HOME/Documents"
 
-notify-send "Git Auto Push" " Starting push for lowercase-named repos in: $PARENT_DIR"
+# notify-send "Git Auto Push" " Starting push for lowercase-named repos in: $PARENT_DIR"
 
 for dir in "$PARENT_DIR"/*/; do
     repo_name=$(basename "$dir")
